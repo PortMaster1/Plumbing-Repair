b37c4c13c9e25e9ee646acba2ef9953e32ee6b76
Dalton Knapp <knapptime_wv@outlook.com>
2025-06-12 21:27:42 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo apt update && sudo apt upgrade -y
 sudo apt install freeradius -y
 
@@ -7,3 +8,25 @@ sudo mysql_secure_installation
 
 sudo systemctl enable freeradius
 sudo systemctl start freeradius
+
+# Setup the AP as a client:
+
+# Prompt for info
+read -p "Enter Access Point IP address: " ip
+read -p "Enter shared secret: " secret
+read -p "Enter shortname: " shortname
+
+# Append to the FreeRADIUS clients.conf file
+cat <<EOF | sudo tee -a /etc/freeradius/3.0/clients.conf > /dev/null
+
+client $shortname {
+    ipaddr = $ip
+    secret = $secret
+    shortname = $shortname
+}
+EOF
+
+echo "AP added as RADIUS client!"
+sudo systemctl restart freeradius
+echo "FreeRADIUS restarted."
+echo "You do not need to run this script again, as it was stricly for install and setup of the FreeRADIUS client."
