a08ac7c6bc80c7678d93ecd2f35446ace8bcd9d9
Dalton Knapp <knapptime_wv@outlook.com>
2025-06-12 21:13:20 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 sudo python3 userlist_generator.py
 sudo rm /etc/freeradius/3.0/mods-config/files/authorize
-sudo copy ./authorize /etc/freeradius/3.0/mods-config/files/authorize
+sudo cp ./authorize /etc/freeradius/3.0/mods-config/files/authorize
