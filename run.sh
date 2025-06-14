63b7e4e0e403f5228d4ee2e7560d147524152587
Dalton Knapp <knapptime_wv@outlook.com>
2025-06-14 12:10:43 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,9 +2,5 @@
 sudo python3 userlist_generator.py
 echo "Successfully gwnerated userlist for FreeRADIUS"
 
-sudo rm -f /etc/freeradius/3.0/mods-config/files/authorize
-sudo mv ./authorize /etc/freeradius/3.0/mods-config/files/authorize
-echo "Successfully moved userlist to FreeRADIUS's folder."
-
 sudo systemctl restart freeradius
 echo "Restarting FreeRADIUS"
