3bb836ca8027acf574a3627065c8b89628c01406
Dalton Knapp <knapptime_wv@outlook.com>
2025-06-15 10:01:32 -0400

    Update db_config.sh

--- db_config.sh
+++ db_config.sh
@@ -1,1 +1,11 @@
 sudo mysql_secure_installation
+
+sudo ln -s /etc/freeradius/3.0/mods-available/sql /etc/freeradius/3.0/mods-enabled/
+# Configure DB in /etc/freeradius/3.0/mods-enabled/sql
+# (use mysql, provide username/password)
+
+mysql -u root -p radius < /etc/freeradius/3.0/mods-config/sql/main/mysql/schema.sql
+
+# Enable SQL in sites-enabled/default
+# Under accounting { and authorize {, add:
+# sql
