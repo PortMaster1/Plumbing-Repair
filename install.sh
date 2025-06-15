9878edc51fafccafc040ad480d0755cfb81afc75
Dalton Knapp <knapptime_wv@outlook.com>
2025-06-14 22:46:26 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,24 +11,4 @@ sudo systemctl start freeradius
 
 chmod +x update_clients.sh
 
-# Setup the AP as a client:
-
-# Prompt for info
-read -p "Enter Access Point IP address: " ip
-read -p "Enter shared secret: " secret
-read -p "Enter shortname: " shortname
-
-# Append to the FreeRADIUS clients.conf file
-cat <<EOF | sudo tee -a /etc/freeradius/3.0/clients.conf > /dev/null
-
-client $shortname {
-    ipaddr = $ip
-    secret = $secret
-    shortname = $shortname
-}
-EOF
-
-echo "AP added as RADIUS client!"
-sudo systemctl restart freeradius
-echo "FreeRADIUS restarted."
 echo "You do not need to run this script again, as it was stricly for install and setup of the FreeRADIUS client."
