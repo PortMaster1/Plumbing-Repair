85f007510a36fcaf0aa2e5ec9e1ca6c25d8a57d1
Dalton Knapp <knapptime_wv@outlook.com>
2025-06-14 22:48:51 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,16 @@ sudo apt install apache2 php php-mysql mariadb-server -y
 sudo mysql_secure_installation
 # Then configure database and install daloRADIUS from GitHub
 
+sudo ln -s /etc/freeradius/3.0/mods-available/sql /etc/freeradius/3.0/mods-enabled/
+# Configure DB in /etc/freeradius/3.0/mods-enabled/sql
+# (use mysql, provide username/password)
+
+mysql -u root -p radius < /etc/freeradius/3.0/mods-config/sql/main/mysql/schema.sql
+
+# Enable SQL in sites-enabled/default
+# Under accounting { and authorize {, add:
+# sql
+
 sudo systemctl enable freeradius
 sudo systemctl start freeradius
 
