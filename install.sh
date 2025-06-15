29fd09f2d12440e33405924ffcd54f9eb3d19199
Dalton Knapp <knapptime_wv@outlook.com>
2025-06-15 12:10:32 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,11 +6,24 @@ sudo apt install apache2 php php-mysql mariadb-server -y
 # Then configure database and install daloRADIUS from GitHub
 
 
+
 sudo systemctl enable freeradius
 sudo systemctl start freeradius
 
 chmod +x update_clients.sh
 
 pip install mysql-connector-python --break-system-packages
+
+cd /var/www/html
+wget https://github.com/lirantal/daloradius/archive/1.3.zip
+unzip 1.3.zip
+mv daloradius-1.3 daloradius
+sudo mysql -u root -p radius < daloradius/contrib/db/fr2-mysql-daloradius-and-freeradius.sql
+sudo mysql -u root -p radius < daloradius/contrib/db/mysql-daloradius.sql
+sudo cp daloradius/library/daloradius.conf.php.sample daloradius/library/daloradius.conf.php
+# Edit DB credentials in config file
+sudo chown -R www-data:www-data daloradius
+sudo chmod 664 daloradius/library/daloradius.conf.php
+sudo systemctl restart freeradius
 
 echo "You do not need to run this script again, as it was stricly for install and setup of the FreeRADIUS client."
