bcafa27c8afbe4b1a4f159fc92956bd8a411a29c
Dalton Knapp <knapptime_wv@outlook.com>
2025-06-15 14:00:51 -0400

    Update userlist_generator.py

--- userlist_generator.py
+++ userlist_generator.py
@@ -1,131 +1,59 @@
-import os, sys, string, secrets
-import subprocess
-import re
+# Created by Dalton Knapp
 
-entry_template = """username Cleartext-Password := "password"
-		Reply-Message := "Welcome to the network!"
-"""
+import os, sys, requests, csv, string, secrets
 
-infile = "/etc/freeradius/3.0/mods-config/files/authorize"
-outfile = "/tmp/authorize"
 
-class Generate:
-	def __init__(self, path="./users.txt"):
-		self._all_accounts = {}
-		self.path = path
-		self.users = []
-		self.accounts = []
-	
-	def generate(self):
-		users = self.get_users()
-		accounts = self.setup_accounts(users)
-		self.write_userfile(accounts)
+users = []
+accounts = []
 
-	def generate_ppsk(self, length=12):
-		characters = string.ascii_letters + string.digits + "!@#S%^&*()-_=+[]{}"
-		return "".join(secrets.choice(characters) for _ in range(length))
-	
-	def get_users(self):
-		self.users.clear()
+def get_users():
 		users = []
 		try:
-			with open(self.path, "r") as f:
-				for line in f:
-					line = line.replace(" ", ".")
-					line = line.lower()
-					line = line.strip()
-					users.append(line)
-			self.users = users.copy()
+			with open("users.csv", "r") as f:
+				reader = csv.reader(file)
+    for row in reader:
+        line = "".join(row)
+								line = line.replace(" ", ".")
+								line = line.lower()
+								line = line.strip()
+								users.append(line)
 			return users
 		except FileNotFoundError as e:
 			print("Error reading file.")
 			raise e
 
-	def setup_accounts(self):
-		self.accounts.clear()
-		self.all_accounts.clear()
+def generate_ppsk(length=12):
+		characters = string.ascii_letters + string.digits + "!@#S%^&*()-_=+[]{}"
+		return "".join(secrets.choice(characters) for _ in range(length))
+
+def create_csv(data):
+	with open('users.csv', mode='w', newline='') as file:
+				writer = csv.writer(file)
+				writer.writerows(data)
+
+def setup_accounts(users):
 		accounts = []
-		for user in self.users:
-			ppsk = self.generate_ppsk()
-			self._all_accounts[user] = ppsk
-			accounts.append(entry_template.replace("username", user).replace("password", ppsk))
+		for user in users:
+			ppsk = generate_ppsk()
+			accounts.append([user, ppsk])
 		for account in accounts:
 			print(account)
-		self.accounts = accounts.copy()
-		return accounts
-	
-	@property
-	def all_accounts(self):
-		if self._all_accounts != {}:
-			return self._all_accounts
-		self.get_users()
-		with open(infile, "r") as f:
-			for line in f:
-				if line.startswith("\t\t"):
-					continue
-				if "Cleartext-Password" in line:
-					username = line.split()[0]
-					password = re.search(r'"([^"]*)"', line).group(1)
-					self._all_accounts[username] = password
-		return self._all_accounts
-	
-	@all_accounts.setter
-	def all_accounts(self, value):
-		if not isinstance(value, dict):
-			raise TypeError("Value must be a dictionary.")
-		self._all_accounts = value
-
-	def write_userfile(self):
-		with open(outfile, "w") as f:
-			f.writelines(self.accounts)
-		subprocess.run(["sudo", "mv", outfile, infile])
-
-
-class Reissue:
-	def __init__(self, generator=None):
-		if not generator:
-			generator = Generate()
-		self.g = generator
-		self.account = None
-	
-	def get_accounts(self):
-		with open(infile, "r") as f:
-			accounts = []
-			for line in f:
-				if line.startswith("\t\t"):
-					continue
-				accounts.append(line)
 		return accounts
 
-	def find_user(self, name):
-		name = name.replace(" ", ".")
-		name = name.lower()
-		name = name.strip()
-		accounts = self.get_accounts()
-		found = None
-		for account in accounts:
-			if name in account:
-				found = account
-		if not found:
-			raise ValueError()
-		self.account = account
-		return account
 
-	def replace_ppsk(self, username, account=None):
-		if not account:
-			account = self.account
-		ppsk = self.g.generate_ppsk()
-		print(f"User '{username}' now has password '{ppsk}'")
-		with open(infile, "r") as inputfile, open(outfile, "w") as outputfile:
-			for line in inputfile:
-				if line.startswith(username + " "):
-					line = re.sub(r'"[^"]*"', f'"{ppsk}"', line)
-				outputfile.write(line)
-		subprocess.run(["sudo", "mv", outfile, infile])
-		return ppsk
+# ----------------------- #
 
 ###
 
 if __name__ == "__main__":
-	g = Generate()
-	g.generate()
+	if len(sys.argv) > 1:
+		if "--setup-accounts" in sys.argv or "-s" in sys.argv:
+			users = get_users()
+			accounts = setup_accounts(users)
+			create_csv(accoubts)
+		elif "--generate-ppsk" in sys.argv or "-g" in sys.argv:
+			new_ppsk = generate_ppsk()
+			print("New ppsk:")
+			print(new_ppsk)
+		else:
+			print("You can reference the README.md if unsure about use of this script.")
